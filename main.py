925ea49e7903dcf79e9a7c9dfad827a33a8b43df
bestcode123 <burgerboy99@icloud.com>
2022-03-02 16:51:10 +0000

    Refactor

--- main.py
+++ main.py
@@ -6,22 +6,65 @@ class RenderImage:
         return [self.mesh_res, self.render_res, self.ray_res]
     def __version__(self):
         return '0.0.1a'
-    def mesh_point_calc(self, indata, closest_num):
-        self.indata = indata
-        in2 = indata
+    def sqcalcfull(self, indata):
+        # Step 1: 2D Mesh Point calculation
+        face  = [
+            [1, 1, 0], # c1
+            [3, 1, 0], # c2
+            [1, 3, 0], # c3
+            [3, 3, 0] # c4 // (NON-REGARD)
+        ]
+        inface = []
+        gridset = []
+        grid_size = (4, 4, 0)
+        ugrid_size = (grid_size[0]+1,grid_size[1]+1,grid_size[2]+1)
+        for z in range(ugrid_size[2]):
+            for y in range(ugrid_size[1]):
+                for x in range(ugrid_size[0]):
+                    gridset.append([x, y, z])
+        for c in gridset:
+            if(face[1-1][0] <= c[0] <= face[2-1][0] and face[1-1][1] <= c[1] <= face[3-1][1]):
+                inface.append(c)
+        # inface = output
+        # Step 2: Point Conversion
+        tdp2 = []
+        for c in inface:
+            tdp1 = []
+            tdp1.append(c[0])
+            tdp1.append(c[1])
+            tdp2.append(tdp1)
+        #print(tdp2)
+        g2 = []
+        for c in gridset:
+            g1 = []
+            g1.append(c[0])
+            g1.append(c[1])
+            g2.append(tdp1)
+        #print(tdp2)
+        # Step 3: Color calculation:
+        white = [255, 255, 255]
+        black = [0, 0, 0]
         out = []
+        for y in range(ugrid_size[1]):
+            xout = []
+            for x in range(ugrid_size[0]):
+                coord = [x, y]
+                w = False
+                for c in tdp2:
+                    if(c == coord):
+                        w = True
+                if(w):
+                    xout.append(white)
+                else:
+                    xout.append(black)
+            out.append(xout)
 
-        for j in range(closest_num):
-            min = in2[0][2]
-            min2 = in2[0]
-            for i in range(len(in2)):
-                if(in2[i][1] < min):
-                    min = in2[i][2]
-                    min2 = in2[i]
-            in2.remove(min2)
-            out.append(min2)
-        return out
+        # Step 4: Rendering:
+        from PIL import Image
+        import numpy as np
+        Image.fromarray(np.uint8(out)).convert('RGB').save('2drend.png')
+        return 0
 
 co = [[1, 5, 0], [4, 5, 0], [3, 2, 1], [3, 3, 1], [1, 1, 0], [4, 1, 0]]
 root = RenderImage(1, (len(co), len(co)))
-print(root.mesh_point_calc(co, 4))
+print(root.sqcalcfull(co))
---
+++ phantom.py
@@ -0,0 +1,75 @@
+# Step 1: 2D Mesh Point calculation
+
+face  = [
+    [1, 1, 0], # c1
+    [3, 1, 0], # c2
+    [1, 3, 0], # c3
+    [3, 3, 0] # c4 // (NON-REGARD)
+]
+
+inface = []
+gridset = []
+
+grid_size = (4, 4, 0)
+ugrid_size = (grid_size[0]+1,grid_size[1]+1,grid_size[2]+1)
+
+for z in range(ugrid_size[2]):
+    for y in range(ugrid_size[1]):
+        for x in range(ugrid_size[0]):
+            gridset.append([x, y, z])
+
+for c in gridset:
+    if(face[1-1][0] <= c[0] <= face[2-1][0] and face[1-1][1] <= c[1] <= face[3-1][1]):
+        inface.append(c)
+
+# inface = output
+
+# Step 2: Point Conversion
+
+tdp2 = []
+
+for c in inface:
+    tdp1 = []
+    tdp1.append(c[0])
+    tdp1.append(c[1])
+    tdp2.append(tdp1)
+
+#print(tdp2)
+
+g2 = []
+
+for c in gridset:
+    g1 = []
+    g1.append(c[0])
+    g1.append(c[1])
+    g2.append(tdp1)
+
+#print(tdp2)
+
+# Step 3: Color calculation:
+
+white = [255, 255, 255]
+black = [0, 0, 0]
+
+out = []
+
+for y in range(ugrid_size[1]):
+    xout = []
+    for x in range(ugrid_size[0]):
+        coord = [x, y]
+        w = False
+        for c in tdp2:
+            if(c == coord):
+                w = True
+        if(w):
+            xout.append(white)
+        else:
+            xout.append(black)
+    out.append(xout)
+
+# Step 4: Rendering:
+
+from PIL import Image
+import numpy as np
+
+Image.fromarray(np.uint8(out)).convert('RGB').save('2drend.png')
